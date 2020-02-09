1b358de014d49fdde2bf930a569c0656ad31372e
NikitaPO <gigzim345@gmai.com>
2020-02-10 00:18:07 +0300

    feature: add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Card, Grid } from "semantic-ui-react";
+import {
+  Container,
+  Card,
+  Grid,
+  Segment,
+  Dimmer,
+  Loader,
+  Image
+} from "semantic-ui-react";
 import BookCard from "../containers/BookCard";
 import Menu from "../containers/Menu";
 import Filter from "../containers/Filter";
@@ -18,20 +26,28 @@ export default class App extends React.Component {
     return (
       <Container>
         <Menu />
-        <Grid columns="two">
-          <Grid.Column width={12}>
-            <Card.Group itemsPerRow={3}>
-              {!isReady
-                ? "Загрузка..."
-                : books.map((book, index) => (
-                    <BookCard key={index} {...book} />
-                  ))}
-            </Card.Group>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Filter setFilter={setFilter} filterBy={filterBy} />
-          </Grid.Column>
-        </Grid>
+        {!isReady ? (
+          <Segment>
+            <Dimmer active inverted>
+              <Loader inverted>Загрузка...</Loader>
+            </Dimmer>
+
+            <Image src="/images/wireframe/short-paragraph.png" />
+          </Segment>
+        ) : (
+          <Grid columns="two">
+            <Grid.Column width={12}>
+              <Card.Group itemsPerRow={3}>
+                {books.map((book, index) => (
+                  <BookCard key={index} {...book} />
+                ))}
+              </Card.Group>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Filter setFilter={setFilter} filterBy={filterBy} />
+            </Grid.Column>
+          </Grid>
+        )}
       </Container>
     );
   }
